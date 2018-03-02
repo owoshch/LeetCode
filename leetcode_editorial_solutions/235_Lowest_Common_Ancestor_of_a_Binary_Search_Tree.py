d228bbc8bf30da58823404c580cc4fa910292026
Fedor Kitashov <fedor.kitashov@phystech.edu>
2018-03-02 11:33:54 -0800

    Lowest Common Ancestor of a Binary Search Tree

--- leetcode_editorial_solutions/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ leetcode_editorial_solutions/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -6,7 +6,25 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
+        if not root:
+            return root
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
+
+
+
+
+
+
+        
+        '''
         left_node, right_node = sorted([p.val, q.val])
         while not (left_node <= root.val <= right_node):
             root = (root.left, root.right)[root.val < left_node]
         return root
+        '''
