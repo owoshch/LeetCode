f52a6ce40b618a02455ecee82990d969fd5d1dc3
Fedor Kitashov <fedor.kitashov@phystech.edu>
2018-06-06 11:13:09 -0700

    solution for hamming distance problem

---
+++ onsite_solutions/461_hamming_distance.py
@@ -0,0 +1,13 @@
+"""
+9 minutes.
+Beats 91.25 % of python3 submissions.
+"""
+
+class Solution:
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return bin(x ^ y).count("1")
