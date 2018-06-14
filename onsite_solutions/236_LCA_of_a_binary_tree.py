7564586f2fc11a5180b00f388f67fcf3f54fa407
Fedor Kitashov <fedor.kitashov@phystech.edu>
2018-06-14 13:08:31 -0700

    LCA assignments

---
+++ onsite_solutions/236_LCA_of_a_binary_tree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        """
+         If the current (sub)tree contains both p and q, 
+         then the function result is their LCA. 
+         If only one of them is in that subtree, 
+         then the result is that one of them.
+        
+        """
+        if root in (None, p, q): return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        return root if left and right else left or right
